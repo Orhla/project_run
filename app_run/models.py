21b23e5c58f8f7dcfe99417b24807225540d6bfb
Evgeniya Kuzechkina <12235260+Orhla@users.noreply.github.com>
2026-03-05 13:37:39 +0300

    Add Run model and register api/runs

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(null=True)
+    athlete = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
 from django.conf import settings
+from .models import Run
+from .serializers import RunSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -10,3 +13,7 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
