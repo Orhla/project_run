70f61e4035447a2c8bf701ab3fe9bef13c43e3df
Evgeniya Kuzechkina <12235260+Orhla@users.noreply.github.com>
2026-03-05 15:07:05 +0300

    Add Coaches and Athletes filter, registered /api/users

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Run
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+
+    def get_type(self, obj):
+        if obj.is_staff:
+            return 'coach'
+        return 'athlete'
--- app_run/views.py
+++ app_run/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from django.conf import settings
 from .models import Run
-from .serializers import RunSerializer
+from django.contrib.auth.models import User
+from .serializers import RunSerializer, UserSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,6 +15,21 @@ def company_details(request):
                'contacts': settings.CONTACTS}
     return Response(details)
 
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        type = self.request.query_params.get('type', None)
+        if type == 'athlete':
+            qs = qs.filter(is_staff=False)
+        elif type == 'coach':
+            qs = qs.filter(is_staff=True)
+        return qs.exclude(is_superuser=True)
