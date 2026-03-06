ac28b6c69514f5a12df292d688da716850360b70
Evgeniya Kuzechkina <12235260+Orhla@users.noreply.github.com>
2026-03-06 13:26:30 +0300

    Add athlete info into runs and activate django debug toolbar

--- project_run/urls.py
+++ project_run/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from app_run.views import RunViewSet, company_details, UserViewSet
@@ -29,4 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
     path('', include(router.urls))
-]
+] + debug_toolbar_urls()
